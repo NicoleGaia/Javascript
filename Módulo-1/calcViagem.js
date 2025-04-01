64883bf9835c0e5ef530c4ecf35eedd42e6bf2d4
NicoleGaia <nicgaia01@gmail.com>
2025-04-01 18:29:54 -0300

    update calcViagem

--- Módulo-1/calcViagem.js
+++ Módulo-1/calcViagem.js
@@ -1,7 +1,13 @@
-var preco = 4.5
+var precoAlcool = 5.79
+var precoGasolina = 6.66
+var tipoCombustivel = 'Gasolina'
 var kmlitro = 10
 var dist = 60
 
-const valor = (dist / kmlitro) * preco
-
-console.log(valor)
+if(tipoCombustivel == 'Etanol'){
+    const valor = (dist / kmlitro) * precoAlcool
+    console.log(`O valor gasto para realizar esta viagem é R$ ${valor.toFixed(2)}`)
+} else{
+    const valor = (dist / kmlitro) * precoGasolina
+    console.log(`O valor gasto para realizar esta viagem é R$ ${valor.toFixed(2)}`)
+}
