245b9e01a943613c589602dda760aa4b3eb12c86
NicoleGaia <nicgaia01@gmail.com>
2025-04-02 20:35:08 -0300

    Update numPar.js

--- 04 - Arrays e Estruturas de Repetição/numPar.js
+++ 04 - Arrays e Estruturas de Repetição/numPar.js
@@ -1,6 +1,8 @@
-for (let i = 1; i < 15; i++) {
-    let par = [i]
-    if(par = (i % 2) == 0){
-        console.log(i)
-}else{}
+const numeros = [42, 7, 19, 88, 3, 56, 27, 14, 65, 91, 34, 72, 8, 50, 11]
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i]
+    if(numero % 2 === 0){
+        console.log(numero)
+}
 }
