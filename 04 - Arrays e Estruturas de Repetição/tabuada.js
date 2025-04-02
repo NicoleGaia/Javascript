2927c66632f8f875e194c2cae4ba5e173aadf148
NicoleGaia <nicgaia01@gmail.com>
2025-04-02 20:17:24 -0300

    Update tabuada.js

--- 04 - Arrays e Estruturas de Repetição/tabuada.js
+++ 04 - Arrays e Estruturas de Repetição/tabuada.js
@@ -1,8 +1,8 @@
-let num = 3
+let num = 5
 
 for (let i = 0; i < 10; i++) {
      const valor = [i]
     result = num * valor
-    console.log(result)
+    console.log(`${num} X ${i} = ${result}`)
 }
 
