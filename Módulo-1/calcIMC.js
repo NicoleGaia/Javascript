7809e5f3f3786b51f111b5dd9dc686fbc12526c5
NicoleGaia <nicgaia01@gmail.com>
2025-04-02 09:12:48 -0300

    update calcIMC

--- Módulo-1/calcIMC.js
+++ Módulo-1/calcIMC.js
@@ -4,12 +4,12 @@ const IMC = peso / (altura * altura)
 
 if(IMC < 18.5){
     console.log(`IMC = ${IMC.toFixed(2)} - Abaixo do peso!`)
-} else if(IMC >= 18.5 && IMC.toFixed(2) < 25){
-    console.log(`IMC = ${IMC} - Peso normal!`)
+} else if(IMC >= 18.5 && IMC < 25){
+    console.log(`IMC = ${IMC.toFixed(2)} - Peso normal!`)
 } else if(IMC >= 25 && IMC < 30){
-    console.log(`IMC = ${IMC} - Acima do peso!`)
+    console.log(`IMC = ${IMC.toFixed(2)} - Acima do peso!`)
 } else if(IMC >= 30 && IMC < 40){
-    console.log(`IMC = ${IMC} - Obeso!`)
+    console.log(`IMC = ${IMC.toFixed(2)} - Obeso!`)
 } else {
-    console.log(`IMC = ${IMC} - Obesidade grave!`)
+    console.log(`IMC = ${IMC.toFixed} - Obesidade grave!`)
 }
