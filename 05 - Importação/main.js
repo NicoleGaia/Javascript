209cdcaa414046badb6298b64318b2e361a5becb
NicoleGaia <nicgaia01@gmail.com>
2025-04-03 10:47:54 -0300

    Update impportações

--- 05 - Importação/main.js
+++ 05 - Importação/main.js
@@ -1,4 +1,14 @@
-const funcoes = require('./funcoes-auxiliares'
+/*const funcoes = require('./funcoes-auxiliares'
 )
 
-console.log(funcoes.gets())
+console.log(funcoes.gets())*/
+
+// Object desctruction
+
+const { gets, print } = require('./maiorNum')
+
+print(gets())
+print(gets())
+print(gets())
+print(gets())
+print(gets())
