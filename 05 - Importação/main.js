48ae38c83036cac2c8de8c25f454f27d658977d7
NicoleGaia <nicgaia01@gmail.com>
2025-04-03 11:09:22 -0300

    Update main.js

--- 05 - Importação/main.js
+++ 05 - Importação/main.js
@@ -7,8 +7,21 @@ console.log(funcoes.gets())*/
 
 const { gets, print } = require('./maiorNum')
 
-print(gets())
-print(gets())
-print(gets())
-print(gets())
-print(gets())
+const numSorteados = []
+
+for (let i = 0; i < 5; i++) {
+    const numSorteado = gets()
+    numSorteados.push(numSorteado)
+    
+}
+let maiorValor = 0
+
+for (let i = 0; i < numSorteados.length; i++) {
+    const numSorteado = numSorteados[i]
+    if(numSorteado > maiorValor){
+        maiorValor = numSorteado
+    }
+}
+
+print(maiorValor)
+print(numSorteados)
