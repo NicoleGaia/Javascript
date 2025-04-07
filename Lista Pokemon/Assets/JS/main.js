7e57fd59ce4f497618b183910ece4eaeeaf8fa0c
NicoleGaia <nicgaia01@gmail.com>
2025-04-07 19:29:08 -0300

    Update

--- Lista Pokemon/Assets/JS/main.js
+++ Lista Pokemon/Assets/JS/main.js
@@ -1,6 +1,4 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
 
 function getPokemonList(pokemon){
     return `
@@ -22,15 +20,8 @@ function getPokemonList(pokemon){
 const pokemonList = document.getElementById('pokemonList')
 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(getPokemonList).join('')
+    pokemonList.innerHTML += newHtml
+})
         
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += getPokemonList(pokemon)
-
-        }
-    })
-    .catch((error) => console.log(error))
